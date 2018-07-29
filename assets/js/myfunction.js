824738d37330b38d240b8f9110a71c60c1d2d358
joytou <1540294142@qq.com>
2018-07-29 11:16:36 +0800

    Fixed js bug.

--- assets/js/myfunction.js
+++ assets/js/myfunction.js
@@ -1,3 +1,6 @@
+---
+layout: null
+---
 /*archive.js*/
 var searchURL=window.location.search;searchURL=searchURL.substring(1,searchURL.length);var targetPageId=searchURL.split("&")[0].split("=")[1];document.write(targetPageId);
 /*cb-search.js*/
