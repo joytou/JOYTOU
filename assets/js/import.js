021559439b9f70ed6cde8e81cdf07dc6f46b7472
joytou <1540294142@qq.com>
2018-08-02 11:09:38 +0800

    Fix: Fixed the import.js file content misspell bug.

--- assets/js/import.js
+++ assets/js/import.js
@@ -1,2 +1,2 @@
-document.write("<script type='text/javascript' src='jquery-3.2.1.min.js'></script>");
-document.write("<script type='text/javascript' src='bootstrap.min.js'></script>");
+window.document.write('<script src="jquery-3.2.1.min.js"></script>');
+window.document.write('<script src="bootstrap.min.js"></script>');
