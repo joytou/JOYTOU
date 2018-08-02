a25b8c7ed50030d102e2096be689554e40911e39
joytou <1540294142@qq.com>
2018-08-02 11:17:31 +0800

    Remove: Del import.js.

--- assets/js/import.js
+++
@@ -1,4 +0,0 @@
----
----
-window.document.write('<script src="{{ "/assets/js/jquery-3.2.1.min.js" | prepend: site.baseurl }}?v={{ site.time | date:"%Y%m%d" }}"></script>');
-window.document.write('<script src="{{ "/assets/js/bootstrap.min.js" | prepend: site.baseurl }}?v={{ site.time | date:"%Y%m%d" }}"></script>');
