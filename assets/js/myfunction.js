e70ba663dbbb55387b95376e4bac37c727bf4789
joytou <1540294142@qq.com>
2018-08-06 00:05:57 +0800

    Still tesy

--- assets/js/myfunction.js
+++ assets/js/myfunction.js
@@ -24,12 +24,10 @@ var converter=new Markdown.Converter();
     url: 'https://api.github.com/repos/{{ site.github_username }}/{{ site.github_repo }}/issues',
     dataType: 'json',
     data:{
-            label: {{ site.github_label }},
-            state: open,
-            filter: create,
-            per_page: 1,
-            page: 1,
-            access_token: 9455ef7c20e35193d772ff6c040e89e66c429e15
+            "label": "{{ site.github_label }}",
+            "state": "open",
+            "filter": "created",
+            "access_token": "9455ef7c20e35193d772ff6c040e89e66c429e15"
         },
     success: function(data) {
 document.getElementById("jumbotrontitle").innerHTML=data[i].title;
@@ -49,7 +47,7 @@ if(document.getElementById("listgroup"))
     url: 'https://api.github.com/repos/{{ site.github_username }}/{{ site.github_repo }}/releases',
     dataType: 'json',
     data:{
-            access_token: 9455ef7c20e35193d772ff6c040e89e66c429e15
+            "access_token": "9455ef7c20e35193d772ff6c040e89e66c429e15"
     },
     success: function(data) {
     for(var i=0;i<data.length;i++){
