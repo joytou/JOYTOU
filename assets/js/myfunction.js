cc529f5ff2864ac40c2c94f0add3eab3e2bd1ef1
joytou <1540294142@qq.com>
2018-08-06 08:15:40 +0800

    Fixed bug that parsing json data error.

--- assets/js/myfunction.js
+++ assets/js/myfunction.js
@@ -36,7 +36,6 @@ var access_token="04b59d3f707f4eb18e"+"c87e0e1beb7fabed86b69c";
 document.getElementById("jumbotrontitle").innerHTML=data[0].title;
 document.getElementById("jumbotroncontent").innerHTML=converter.makeHtml(data[0].body);
 document.getElementById("jumbotronurl").href=data[0].html_url;
-document.getElementById("jumbotroncontent").innerHTML=JSON.stringify(data,null,"\t");
     },
     statusCode: {
      404: function() {
