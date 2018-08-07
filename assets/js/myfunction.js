520ef6a1caea196d55632f8e1c66034d56270b41
joytou <1540294142@qq.com>
2018-08-07 22:56:26 +0800

    Still test login

--- assets/js/myfunction.js
+++ assets/js/myfunction.js
@@ -35,7 +35,7 @@ if(document.getElementById("listgroup")){var accesstoken="04b59d3f707f4eb18e"+"c
 if(window.location.search){
 var logincode=window.location.search.split("=")[1];
 $.ajax({
-        dataType:'json',
+        dataType:'text',
         url: 'https://github.com/login/oauth/access_token',
         data:{"client_id":"760be777aaf934af6eca","client_secret":"144e00bb0f7fc45f732ba405d"+"8b7368572e287c1","code":logincode},
         success:function(data){
